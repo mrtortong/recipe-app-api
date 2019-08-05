9bce0ae644123207d339aef0083029a64ec3e7ee
MrTorTong <archanai.technology@gmail.com>
2019-08-05 12:42:11 +0700

    Added user models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,6 +12,7 @@ class ModelTests(TestCase):
         ) 
 
         self.assertEqual(user.email, email)
+        self.assetTrue(user.check_password(password))
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
