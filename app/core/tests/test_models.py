1b48fae7a81c548dd1b03bd32bbb4bf73916d6e5
MrTorTong <archanai.technology@gmail.com>
2019-08-05 12:48:57 +0700

    Add custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,7 +21,7 @@ class ModelTests(TestCase):
     
     def test_new_user_email_normalized(self):
         	"""Test the email for a new user is normalized"""
-	    email = 'test@abcd.com'
+	    email = 'test@ABCDEF.com'
 	    user = get_user_model().objects.create_user(email, 'test123')
 	
 	    self.assertEqual(user.email, email.lower())
